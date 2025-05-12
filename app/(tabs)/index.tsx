021a226dcc930fc81b70c8c5bdd9c1cc50c21b6c
MatiasValdesdesdes <2302060@utrivieramaya.edu.mx>
2025-05-12 18:41:52 -0500

    InventarioReactNative

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,78 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React from 'react'
+import {Link} from 'expo-router'
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+export default function index() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+<ScrollView>
+<View style={styles.contenedorprincipal}></View>
+
+    <View>
+      <Text style={styles.titulo}>Bien venido</Text>
+      <Text style={{textAlign:'center',fontSize:40, color:'purple'}}>Login</Text>
+    </View>
+    <View style={{marginTop:35}}>
+    <TextInput style={styles.input} placeholder='Usuario'/>
+    <TextInput style={styles.input} placeholder='Password'
+    secureTextEntry/>
+    </View>
+
+ <View>
+  <View style={{flexDirection:'row', marginTop:20}}>
+      <TouchableOpacity style={styles.button1}>Login</TouchableOpacity>
+  </View>
+  <View>
+      <View style={{flexDirection:'row', marginTop:20}}>
+        <TouchableOpacity style={styles.button2}>Register</TouchableOpacity>
+      </View>
+  </View>
+
+  <Link href="/crearcuenta">Crear cuenta</Link>
+</View>
+</ScrollView>
+  )
 }
 
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  contenedorprincipal:{
+    marginTop:120,
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    },
+  titulo:{
+    fontSize:50,
+    textAlign:'center',
+    fontWeight:'bold',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input:{
+    margin:8,
+    backgroundColor:'white',
+    fontSize:25,
+    borderRadius:8,
+    padding:6,
+    borderWidth:2,
+    borderColor:'purple',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  contenedorbutton:{
+    marginTop:20,
+    padding:8
   },
-});
+  button1:{
+  backgroundColor:'purple',
+  color:'white',
+  padding:10,
+  borderRadius:30,
+  width:150,
+  textAlign:'center',  
+  },
+  button2:{
+  backgroundColor:'#9B7EBD',
+  color:'white',
+  padding:10,
+  borderRadius:30,
+  width:150,
+  textAlign:'center',  
+  },
+})
